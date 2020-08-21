5a5a8097677576d730dbee8c8e1053a1dceb1c3f
Adriel Adonis <arodriguez32@fisher.edu>
2020-08-21 17:20:41 -0400

    saving edit.jsx controllers/shoes.js

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -62,7 +62,7 @@ router.post("/", (req, res)=> {
 // Edit
 router.get("/:id/edit", (req, res)=> {
     Shoes.findById(req.params.id, (error, foundShoes)=>{
-        res.render("/Edit", {
+        res.render("Edit", {
             shoe: foundShoes
         })
     });
--- views/Edit.jsx
+++ views/Edit.jsx
@@ -7,7 +7,7 @@ class Edit extends React.Component {
             <div>
                 <h1>Stock X's Edit Page</h1>
                 <form action={`/shoes/${_id}?_method=PUT`} method="POST">
-                    Name: <input type="text" name="title" defaultValue={name}/><br />
+                    Name: <input type="text" name="name" defaultValue={name}/><br />
                     Description: <input type="text" name="description" defaultValue={description}/><br />
                     areShoesInStock: <input type="checkbox" name="areShoesInStock" checked={areShoesInStock}/>
                     <input type="submit" name="" value="Submit Changes"/>
