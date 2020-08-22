a8e9f06ae6c7ab70f9ede5eb5b4dfe55aac7ad34
Adriel Adonis <arodriguez32@fisher.edu>
2020-08-22 01:09:30 -0400

    fixed bug for Edit route

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Shoes = require('../models/shoes.js');
-
 // Index Route
 router.get('/', (req, res)=>{
     Shoes.find({}, (error, allShoes)=> {
@@ -10,25 +9,20 @@ router.get('/', (req, res)=>{
         });
     });
 });
-
 // New
 router.get('/new', (req, res)=>{
     res.render('New');
     console.log('check');
 })
-
 // Delete/DESTROY
 router.delete("/:id", (req, res)=> {
     Shoes.findByIdAndRemove(req.params.id, (error, shoe)=>{
         res.redirect("/shoes");
     });
 });
-
-
 // Update
 router.put("/:id", (req, res)=> {
     req.body.areShoesInStock = req.body.areShoesInStock === "on" ? `true` : `false`;
-
     // Update shoe document
     Shoes.findByIdAndUpdate(
         req.params.id,
@@ -41,7 +35,6 @@ router.put("/:id", (req, res)=> {
         }
     )
 })
-
 // Create 
 router.post("/", (req, res)=> {
     if(req.body.areShoesInStock === "on") {
@@ -56,33 +49,14 @@ router.post("/", (req, res)=> {
         console.log(createdShoes);
     });
 });
-
-
-//Edit
-router.get('/:id/edit', (req, res)=>{
+// Edit
+router.get("/:id/edit", (req, res)=> {
     Shoes.findById(req.params.id, (error, foundShoes)=>{
-      if(error){
-        res.status(500).send({
-          error: error.message
+        res.render("Edit", {
+            shoe: foundShoes
         })
-      } else {
-        res.render('Edit', {
-          shoe: foundShoes
-        })
-      }
     });
-  });
-
-
-// // Edit
-// router.get("/:id/edit", (req, res)=> {
-//     Shoes.findById(req.params.id, (error, foundShoes)=>{
-//         res.render("Edit", {
-//             shoe: foundShoes
-//         })
-//     });
-// });
-
+});
 // Show
 router.get("/:id", (req, res)=>{
     Shoes.findById(req.params.id, (error, foundShoes)=>{
@@ -91,5 +65,4 @@ router.get("/:id", (req, res)=>{
         });
     });
 });
-
 module.exports = router;
