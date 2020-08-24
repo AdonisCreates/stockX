3b0ea78599ec8404ec1957e151bb84fb2ef8d799
Adriel Adonis <arodriguez32@fisher.edu>
2020-08-24 11:56:19 -0400

    fixed mongoDB typo

--- views/components/Default.jsx
+++ views/components/Default.jsx
@@ -3,57 +3,44 @@ const React = require("react");
 class Default extends React.Component {
   render() {
     return (
-      <html>
-        <head>
-          <link rel="stylesheet" href="./css/main.css" />
-          <meta charset="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>StockX Mock Store</title>
-        </head>
-        <body>
-          <div>
-            {/* // HEADER THAT WILL REPLICATE STOCKX'S HEADER MUST PUT IN DIV ALWAYS//  */}
-            <header
-              style={{ backgroundColor: "green", justifyContent: "center" }}
-            >
-              <h1 style={{ textAlign: "center", color: "whitesmoke" }}>
-                StockX
-              </h1>
-              {/* THE H1 GOT FORMATTED WEIRLDLY... VS CODE AND I GOT BEEF */}
-              <h1
-                style={{
-                  textAlign: "center",
-                  color: "whitesmoke",
-                  backgroundColor: "olive",
-                }}
-              >
-                Buy & Sell
-              </h1>
-              <h1
-                style={{
-                  textAlign: "center",
-                  color: "whitesmoke",
-                  backgroundColor: "black",
-                }}
-              >
-                Authentic Sneakers
-              </h1>
-            </header>
-            {/* // NAV BAR WILL BE RIGHT HERE //  */}
-            <div class="topnav">
-              <a class="active" href="#home">
-                Home
-              </a>
-              <a href="#news">News</a>
-              <a href="#contact">Contact</a>
-              <a href="#about">About</a>
-            </div>
-            {/* NAV BAR ENDS HERE */}
+<html>
+<head>
+    <link rel="stylesheet" href="./css/main.css"/>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>StockX Mock Store</title>
+</head>
+<body>
+      <div>
+        {/* // HEADER THAT WILL REPLICATE STOCKX'S HEADER MUST PUT IN DIV ALWAYS//  */}
+        <header style={{ backgroundColor: "green", justifyContent: "center" }}>
+          <h1 style={{ textAlign: "center", color: "whitesmoke" }}>StockX</h1>
+          {/* THE H1 GOT FORMATTED WEIRLDLY... VS CODE AND I GOT BEEF */}
+          <h1
+            style={{
+              textAlign: "center",
+              color: "whitesmoke",
+              backgroundColor: "olive",
+            }}
+          >
+            Buy & Sell
+          </h1>
+          <h1 style={{ textAlign: "center", color: "whitesmoke", backgroundColor: "black" }}>
+            Authentic Sneakers
+          </h1>
+        </header>
+        {/* // NAV BAR WILL BE RIGHT HERE //  */}
+        <div class="topnav">
+          <a class="active" href="#home">
+            Home
+          </a>
+          <a href="#news">News</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+        </div>
+        {/* NAV BAR ENDS HERE */}
 
-            {/* <ol style={{ display: "flex", flexWrap: "wrap-reverse" }}>
+        {/* <ol style={{ display: "flex", flexWrap: "wrap-reverse" }}>
           {this.props.fruits.map((fruit, i) => {
             return (
               <ul key={fruit._id}>
@@ -69,14 +56,25 @@ class Default extends React.Component {
             );
           })}
         </ol> */}
-            <nav>
-              {/* Create link to make a new shoe */}
-              <a href="/fruits/new">Create New Shoes</a>
-            </nav>
-            <div>{this.props.children}</div>
-          </div>
-        </body>
-      </html>
+        <nav>
+          {/* Create link to make a new shoe */}
+          <a href="/shoes/new">Create New Shoes</a>
+          <br />
+
+          {/* Buy Button I could not figure out. Along with the inventory and subtraction. New link will redirect user to creating rather than buying. */}
+          <a href="/shoes/new">Buy New Shoes</a>
+          <br />
+
+          {/* Delete link redirects to new page. Will use logic from w06d01 to a functional delete button. */}
+        </nav>
+        <div>
+        {this.props.children}
+        </div>
+        
+      </div>
+      </body>
+    </html>
+
     );
   }
 }
